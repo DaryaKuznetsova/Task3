4a8dc1e0b3053dbea5ecc1fcf5b6f4e997c6a125
Дарья Кузнецова <dakuznetsova_3@edu.hse.ru>
2019-05-22 23:20:57 +0500

    condition changed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -38,9 +38,10 @@ namespace Task3
                 Console.WriteLine("Введите y");
                 y = ReadAnswer();
 
-                bool t1 = y > 0 && x >= -1.5 && y <= -x;
-            bool t2 = x <= 1 && x >= -2 && y <= 2 * x + 3 && y >= 1.0 / 3 * x - 1.0 / 3 && y <= 0;
-                bool r = t1 || t2;
+            bool t3 = y >= 1.0 / 3 * x - 1.0 / 3;
+                bool t1 = y <= 2*x+3  && y <= -x;
+            bool t2 =  x >= 0 &&  y <= 0;
+                bool r = (t1 || t2)&&t3;
                 Console.WriteLine(r);
            
 
